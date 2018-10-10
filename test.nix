68b0b1ce595429b11be539e9bf8043d85fa6f1c4
Luke Clifton <Luke.Clifton@cba.com.au>
2018-10-10 09:46:44 +0800

    Test multiple ghc versions

---
+++ test.nix
@@ -0,0 +1,9 @@
+{nixpkgs ? import <nixpkgs> {}}:
+with nixpkgs;
+let
+  compilers = [ "ghc802" "ghc822" "ghc843" "ghc861" ];
+
+  buildFor = ghc: haskell.packages."${ghc}".callPackage ./iterm-show.nix {};
+
+in
+  lib.genAttrs compilers buildFor
