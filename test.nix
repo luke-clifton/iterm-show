4aff2b10a0ad41f74015e8252ad148c9ccf3f4a3
Jonathan Merritt <j.s.merritt@gmail.com>
2019-09-27 20:41:05 +1000

    Updates for GHC-8.8
    - Increase maximum bound on base.
    - Add "ghc881" to the list of Nix Haskell versions.

--- test.nix
+++ test.nix
@@ -1,7 +1,7 @@
 {nixpkgs ? import <nixpkgs> {}}:
 with nixpkgs;
 let
-  compilers = [ "ghc802" "ghc822" "ghc843" "ghc861" ];
+  compilers = [ "ghc802" "ghc822" "ghc843" "ghc861" "ghc881" ];
 
   buildFor = ghc: haskell.packages."${ghc}".callPackage ./iterm-show.nix {};
 
